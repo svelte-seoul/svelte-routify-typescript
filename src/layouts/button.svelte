838ab3d1fb1ae6ce59507ba626e8f2d43f5cc505
hyochan <dooboolab@gmail.com>
2021-12-05 21:04:13 +0900

    Add sample svelte pages

---
+++ src/layouts/button.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  function handleClick() {
+    dispatch("click");
+  }
+</script>
+
+<button on:click={handleClick}>
+  <slot />
+</button>
+
+<style>
+  button {
+    background-color: var(--primary);
+    color: var(--text);
+    border: none;
+    border-radius: 4px;
+    padding: 0.5rem;
+    text-transform: uppercase;
+  }
+</style>
---
+++ src/pages/intro.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Button from "../layouts/button.svelte";
+  import { getContext } from "svelte";
+</script>
+
+<div class="container">
+  <p>HELLO</p>
+  <Button>Change Theme</Button>
+</div>
+
+<style lang="postcss">
+</style>
