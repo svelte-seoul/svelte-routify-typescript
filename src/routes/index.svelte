e4619f4cfe5fbc6fe32f5288c2b0553e11377a01
hyochan <dooboolab@gmail.com>
2021-12-06 16:56:10 +0900

    Add sample svgs and route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,22 @@
 <style lang="postcss">
   .container {
+    justify-self: center;
+    width: 30%;
     color: var(--text);
   }
 </style>
 
 <script lang="ts">
   import {_} from 'svelte-i18n';
-  import Button from '../layouts/button.svelte';
-  import {userStore} from '../stores/userStore';
-  import {getContext} from 'svelte';
-  import type {ThemeStore} from 'svelte-theme';
-
-  const {changeThemeType} = getContext<ThemeStore>('svelte-theme');
-
-  const {fullname, setName} = userStore;
-
-  function handleClick() {
-    changeThemeType();
-  }
-
-  function getUser() {
-    setName('Hyo', 'Jang');
-  }
 </script>
 
 <div class="container">
   <p>{$_('index.title')}</p>
-  <Button on:click={handleClick}>Change Theme</Button>
-  <Button on:click={getUser}>Get User</Button>
-  <p>{$fullname}</p>
 
   <ul>
+    <li>
+      Go to <a href="/sign-in">/sign-in</a>
+    </li>
     <li>
       Go to <a href="/temp">/temp</a>
     </li>
